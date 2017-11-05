b1c0b53ae0dc3e8b9eef31928742914b0877821d
Yang Liu <jeremyrobturtle@gmail.com>
2017-11-04 21:29:39 -0700

    instll iterm2

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,6 +64,13 @@ package { 'git-extras': }
 package { 'tree': }
 
 ####################
+# Common GUI
+####################
+package { 'iterm2':
+    provider => brewcask,
+}
+
+####################
 # Github
 ####################
 # TODO: setup https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/#adding-your-ssh-key-to-the-ssh-agent
