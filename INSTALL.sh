12e59644740ce2466ea99d74f30a005f5740bae3
Yang Liu <jeremyrobturtle@gmail.com>
2017-11-05 14:52:44 -0800

    install script fail fast

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,7 +6,7 @@ function installed {
 installed brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 installed bundle || gem install bundler
-bundle install --without development test
-librarian-puppet install
+bundle install --without development test && \
+librarian-puppet install && \
 puppet apply manifests/site.pp --modulepath=modules/
 
