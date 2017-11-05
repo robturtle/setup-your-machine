4f0c0a6e57b6b8193db05be217872f70121b2076
Yang Liu <jeremyrobturtle@gmail.com>
2017-11-04 22:42:10 -0700

    conditionally install packages

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,10 @@
-# install homebrew
-which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+function installed {
+  which $1 >/dev/null
+}
 
-gem install bundler
+# install homebrew
+installed brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+installed bundle || gem install bundler
 bundle install --without development test
 
